7a30087b4a160980a07b6f23687aa313a0ab3eac
Adithya <palavallisaditya@gmail.com>
2026-08-22 08:18:57 +0530

    Third commit - User model , Registration , Password hashing , Login , JWT generation , at this stage role is by default Student role

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const client = new MongoClient(process.env.MONGO_URI, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      },
-    });
-
-    await client.connect();
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB connected successfully");
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
 
 const app = express();
-const connectDB = require("./config/db");
-const PORT = 5000;
 
-connectDB(); // Connect to MongoDB
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+
 // Test route
 app.get("/", (req, res) => {
   res.json({
-    message: "CampusNotes API is running successfully",
+    message: "CampusNotes API is running",
   });
 });
 
